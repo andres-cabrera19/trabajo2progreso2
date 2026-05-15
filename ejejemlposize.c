422d69307fdae097de82f9297f983fd7b0a50cf8
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-15 14:46:55 -0500

    segundo commit ejercicicio 2

--- ejejemlposize.c
+++ ejejemlposize.c
@@ -2,6 +2,9 @@
 int main(int argc, char const *argv[])
 {
    int i = sizeof(int);
-   printf("%d byte(s)",i);
+   printf("%d byte(s)\n",i);
+   int arreglo[]={3,6,9,12,15,18};
+   int j = sizeof arreglo;
+   printf("%d byte(s)",j);
    return 0;
 }
