0d436c3da3685cfda774f43b7376b22c4646d057
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-15 14:24:22 -0500

    correccion

--- arreglo.c
+++ arreglo.c
@@ -4,4 +4,12 @@ int main (int argc, char *argv []) {
     int arreglo []={3,6,9,12,15,8};
     int *ptr;
     ptr = &arreglo[0]; //ptr = arreglo;  
+    
+    for (int i = 0; i < 6; i++)
+    {
+        printf("arreglo[%d] = %d",i,  arreglo[i]);
+        printf("%p\n", ptr);
+    }
+
+    return 0;
 }
