acc4a2ba88afc3e1d1edd10df1f6a0702a50e1b2
Andres Cabrera <andres.cabrera.calupina@udla.edu.ec>
2026-05-15 14:30:27 -0500

    navegacion por el vector con punteros

--- arreglo.c
+++ arreglo.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
-int main (int argc, char *argv []) {
-    int arreglo []={3,6,9,12,15,8};
-    int *ptr;
-    ptr = &arreglo[0]; //ptr = arreglo;  
-    
-    for (int i = 0; i < 6; i++)
-    {
-        printf("arreglo[%d] = %d",i,  arreglo[i]);
-        printf("%p\n", *ptr);
-    }
-
-    return 0;
+int main (int argc, char *argv[]) {
+   int arreglo[]={3,6,9,12,15,18};
+   int *ptr;
+   ptr = &arreglo[0];      //ptr = arreglo;
+   for (int i = 0; i < 6; i++)
+   {
+       printf("arreglo[%d] = %d ",i, arreglo[i]);
+       printf("%d\n", *(ptr+i));
+   }
+   return 0;
 }
